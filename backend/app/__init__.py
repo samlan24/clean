b564a64c8c488b9e20118ac1653439e75ccc8e0c
samlan24 <muchemionline@gmail.com>
2024-10-02 17:22:35 +0300

    adding chrome extension to cors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,7 +3,11 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, origins=['http://localhost:5173'], supports_credentials=True)
+    CORS(app, origins=[
+    'http://localhost:5173',
+    'chrome-extension://aifcpofagcmbfpmaepakfiplanjgeegm'
+], supports_credentials=True)
+
 
     # Register the blueprint
     from .extension import extension
